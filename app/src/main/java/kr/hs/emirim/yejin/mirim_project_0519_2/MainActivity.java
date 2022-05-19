cd70678b70fd37212c5b5228ce961623ddcd18ee
이 예진 <s2108@e-mirim.hs.kr>
2022-05-19 19:56:12 +0900

    Time_DataPicker

---
+++ app/src/main/java/kr/hs/emirim/yejin/mirim_project_0519_2/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.yejin.mirim_project_0519_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.time_datepicker);
+    }
+}
